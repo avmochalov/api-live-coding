311ea789e95a788fd4b4cd8b3520303b5128a56b
avmochalov <al.mochalov93@gmail.com>
2023-04-24 20:44:01 +0300

    Добавляет форму регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -1,9 +1,14 @@
 import { loginUser } from "../api.js";
 
 export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
-    const appHtml = `<div class="form">
-    <h3 class="form-title">Форма входа</h3>
+    let isLoginMode = false;
+    function renderForm() {
+        const appHtml = `<div class="form">
+    <h3 class="form-title">Форма ${isLoginMode ? 'входа' : 'регистрации'}</h3>
     <div class="form-row">
+    ${isLoginMode ? '' : `Имя
+    <input type="text" id="name-input" class="input" placeholder="Логин" /> <br>`}
+        
         Логин
         <input type="text" id="login-input" class="input" placeholder="Логин" />
         <br>
@@ -11,19 +16,27 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
         <input type="password" id="pwd-input" class="input" placeholder="Пароль" />
     </div>
     <br />
-    <button class="button" id="login-button">Войти</button>
+    <button class="button" id="login-button">${isLoginMode ? 'Войти' : 'Зарегистрироваться'}</button>
+    <button class="button" id="toggle-button">Перейти ${isLoginMode ? 'к регистрации' : 'ко входу'}</button>
 </div>`
-    appEl.innerHTML = appHtml;
-    document.getElementById('login-button').addEventListener('click', () => {
-        let login = document.getElementById('login-input').value;
-        let password = document.getElementById('pwd-input').value;
+        appEl.innerHTML = appHtml;
+        document.getElementById('login-button').addEventListener('click', () => {
+            let login = document.getElementById('login-input').value;
+            let password = document.getElementById('pwd-input').value;
 
-        loginUser({ login: login, password: password })
-            .then((user) => {
-                setToken(`Bearer ${user.user.token}`);
-                fetchTodosAndRender();
-            }).catch((error) => {
-                alert(error.message)
-            })
-    });
+            loginUser({ login: login, password: password })
+                .then((user) => {
+                    setToken(`Bearer ${user.user.token}`);
+                    fetchTodosAndRender();
+                }).catch((error) => {
+                    alert(error.message)
+                })
+        });
+
+        document.getElementById('toggle-button').addEventListener('click', () => {
+            isLoginMode = !isLoginMode;
+            renderForm();
+        })
+    }
+    renderForm();
 }
